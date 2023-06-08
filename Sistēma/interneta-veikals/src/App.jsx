519a5172b3014808f6f6115270951e278aab6971
Karlis <lacitiskarlis@gmail.com>
2023-06-08 09:35:22 +0300

    Pievinota meklēšanas funkcionalitāte

--- Sistēma/interneta-veikals/src/App.jsx
+++ Sistēma/interneta-veikals/src/App.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './Components/Client pages/HomePage';
-import React from 'react';
-import LoginPage from './Components/Login/LoginPage';
-import AdminLogin from './Components/Admin/Admin login/AdminLogin';
-import AdminHome from './Components/Admin/Admin Pages/Home/AdminHome';
-import Database from './Components/Admin/Admin Pages/Database/database';
-import Edit from './Components/Admin/Admin Pages/Database/edit';
-import Create from './Components/Admin/Admin Pages/Database/create';
-import { RequireAuth } from 'react-auth-kit';
-import { CssBaseline } from '@mui/material';
-import ItemPage from './Components/Client pages/ItemPage';
-import Cart from './Components/Client pages/Cart';
-import Payment from './Components/Client pages/Payment';
-import Katalogs from './Components/Client pages/katalogs';
-import RegisterPage from './Components/Register/RegisterPage';
-import Categories from './Components/Client pages/Categories';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./Components/Client pages/HomePage";
+import React from "react";
+import LoginPage from "./Components/Login/LoginPage";
+import AdminLogin from "./Components/Admin/Admin login/AdminLogin";
+import AdminHome from "./Components/Admin/Admin Pages/Home/AdminHome";
+import Database from "./Components/Admin/Admin Pages/Database/database";
+import Edit from "./Components/Admin/Admin Pages/Database/edit";
+import Create from "./Components/Admin/Admin Pages/Database/create";
+import { RequireAuth } from "react-auth-kit";
+import { CssBaseline } from "@mui/material";
+import ItemPage from "./Components/Client pages/ItemPage";
+import Cart from "./Components/Client pages/Cart";
+import Payment from "./Components/Client pages/Payment";
+import Katalogs from "./Components/Client pages/katalogs";
+import RegisterPage from "./Components/Register/RegisterPage";
+import Categories from "./Components/Client pages/Categories";
 
 function App() {
   return (
@@ -28,8 +28,9 @@ function App() {
           <Route exact path="/admin" element={<AdminLogin />} />
           <Route exact path="/cart" element={<Cart />} />
           <Route exact path="/payment" element={<Payment />} />
-          <Route exact path="/katalogs/:id" element={<Katalogs />} />
           <Route exact path="/katalogs" element={<Katalogs />} />
+          <Route exact path="/katalogs/:id" element={<Katalogs />} />
+          <Route exact path="/katalogs/termins/:id" element={<Katalogs />} />
           <Route exact path="/register" element={<RegisterPage />} />
           <Route exact path="/kategorijas" element={<Categories />} />
           <Route
--- Sistēma/interneta-veikals/src/Components/Client pages/katalogs.jsx
+++ Sistēma/interneta-veikals/src/Components/Client pages/katalogs.jsx
@@ -1,29 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import { Container, Grid, Typography } from '@mui/material';
-import CardComp from '../Card/Card';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { Container, Grid, Typography } from "@mui/material";
+import CardComp from "../Card/Card";
+import axios from "axios";
+import { useLocation, useParams } from "react-router-dom";
 
 const Katalogs = () => {
   const [data, setData] = useState([{}]);
   const params = useParams();
   const [kategorija, setkategorija] = useState(false);
+  const [meklesana, setmeklesana] = useState(false);
+  const [katalogs, setkatlogs] = useState(false);
+  const location = useLocation();
+  const [searchTerm, setsearchTerm] = useState();
 
   useEffect(() => {
-    if(Object.keys(params).length === 0){
+    if (
+      Object.keys(params).length === 0 &&
+      !location.pathname.includes("/katalogs/termins/")
+    ) {
+      setkatlogs(true);
       FetchData();
       setkategorija(false);
-    }else{
-      FetchKategoryItems();
+      setmeklesana(false);
+    } else if (location.pathname.includes("/katalogs/termins/")) {
+      setmeklesana(true);
+      FetchData();
+      setsearchTerm(params.id);
+      setkatlogs(false);
+      setkategorija(false);
+    } else {
       setkategorija(true);
+      FetchKategoryItems();
+      setmeklesana(false);
+      setkatlogs(false);
     }
-  }, []);
+  });
+  //Needs to be done on every render otherwise its not possible to switch to a different search method if the page is not reloaded.
+
+  let tempObj;
+  function findSearchItems(item) {
+    let name = item.nosaukums.toLowerCase();
+    let sTerm = searchTerm.toLowerCase();
+    if (name.includes(sTerm)) {
+      return item.nosaukums;
+    }
+  }
 
   const FetchKategoryItems = async () => {
     try {
-      const res = await axios.get(`http://localhost:5001/kategorijasPreces/${params.id}`);
+      const res = await axios.get(
+        `http://localhost:5001/kategorijasPreces/${params.id}`
+      );
       setData(res.data);
     } catch (err) {
       console.log(err);
@@ -33,7 +62,12 @@ const Katalogs = () => {
   const FetchData = async () => {
     try {
       const res = await axios.get(`http://localhost:5001/visasPreces`);
-      setData(res.data);
+      if (meklesana) {
+        tempObj = res.data.filter(findSearchItems);
+        setData(tempObj);
+      } else {
+        setData(res.data);
+      }
     } catch (err) {
       console.log(err);
     }
@@ -44,22 +78,37 @@ const Katalogs = () => {
       <Header />
       <Container
         sx={{
-          m: '1rem auto',
-          height: '10rem',
-          display: 'flex',
-          backgroundColor: '#201e66',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: '.5rem',
+          m: "1rem auto",
+          height: "10rem",
+          display: "flex",
+          backgroundColor: "#201e66",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: ".5rem",
         }}
       >
-        <Typography sx={{ fontSize: '2.5rem' }}>{kategorija ? `Preces zem kategorijas ${data[0].kategorija}` : "Katalogs"}</Typography>
+        <Typography sx={{ fontSize: "2.5rem" }}>
+          {meklesana && `Meklēt ${searchTerm}`}
+          {kategorija && `Preces zem kategorijas ${data[0].kategorija}`}
+          {katalogs && "Katalogs"}
+        </Typography>
       </Container>
-      <Grid container sx={{ display: 'flex', justifyContent: 'center', gap: '1rem' }}>
+      <Grid
+        container
+        sx={{ display: "flex", justifyContent: "center", gap: "1rem" }}
+      >
         {data
           ? data.map((key, index) => {
               return (
-                <Grid key={index} item={true} xs={11.8} sm={5.8} md={5.8} lg={2.8} xl={2.8}>
+                <Grid
+                  key={index}
+                  item={true}
+                  xs={11.8}
+                  sm={5.8}
+                  md={5.8}
+                  lg={2.8}
+                  xl={2.8}
+                >
                   <CardComp
                     key={index}
                     title={key.nosaukums}
@@ -71,7 +120,7 @@ const Katalogs = () => {
                 </Grid>
               );
             })
-          : ''}
+          : ""}
       </Grid>
 
       <Footer />
