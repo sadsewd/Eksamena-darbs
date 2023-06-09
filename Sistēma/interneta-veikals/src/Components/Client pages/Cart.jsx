08d043b127d48181a82b2319e6bf30b2161edd1d
Karlis <lacitiskarlis@gmail.com>
2023-06-09 12:07:10 +0300

    Warning fix

--- Sistēma/interneta-veikals/src/Components/Client pages/Cart.jsx
+++ Sistēma/interneta-veikals/src/Components/Client pages/Cart.jsx
@@ -152,7 +152,7 @@ const Cart = () => {
                         </TableRow>
                       );
                     })
-                  : ""}
+                  : null}
               </TableBody>
             </Table>
           </TableContainer>
